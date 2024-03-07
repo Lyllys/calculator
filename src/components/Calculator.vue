de03e040d0fa01d5d83cbc7c727819cc0fef4657
Lyllys de Souza Galhardo <lyllysgalhardo@gmail.com>
2024-03-06 20:46:06 -0500

    Creating operators methods and result method, and adding listener to these buttons.

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,31 +4,33 @@
     <div @click="clear" class="btn">AC</div>
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
-    <div class="btn operator">÷</div>
+    <div @click="divide" class="btn operator">÷</div>
     <div @click="append('7')" class="btn">7</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('9')" class="btn">9</div>
-    <div class="btn operator">x</div>
+    <div @click="divide" class="btn operator">x</div>
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')" class="btn">6</div>
-    <div class="btn operator">-</div>
+    <div @click="minus" class="btn operator">-</div>
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div class="btn operator">+</div>
+    <div @click="add" class="btn operator">+</div>
     <div @click="append('0')" class="btn zero">0</div>
     <div @click="dot" class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div @click="equal" class="btn operator">=</div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'Calculator',
   data() {
     return {
-      current: ''
+      previous: null,
+      current: '',
+      operator: null,
+      operatorCliked: false
     };
   },
   methods: {
@@ -42,17 +44,45 @@ export default {
       this.current = `${parseFloat(this.current) / 100}`;
     },
     append(number) {
+      if(this.operatorCliked) {
+        this.current = '';
+        this.operatorCliked = false;
+      }
       this.current = `${this.current}${number}`;
     },
     dot() {
       if(this.current.indexOf('.') === -1) {
         this.append('.');
       }
+    },
+    setPrevious(){
+      this.previous = this.current;
+      this.operatorCliked = true;
+    },
+    divide() {
+      this.operator = (a,b) => a / b;
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (a,b) => a * b;
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a,b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a,b) => a + b;
+      this.setPrevious();
+    },
+    equal() {
+      const totalCurrent = this.operator(parseFloat(this.current), parseFloat(this.previous));
+      this.current = `${totalCurrent}`;
+      this.previous = null;
     }
   }
 };
 </script>
-
 <style scoped>
 .calculator {
   margin: 0 auto;
@@ -62,22 +92,18 @@ export default {
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
 }
-
 .display {
   grid-column: 1/5;
   background-color: #333;
   color: white;
 }
-
 .zero {
   grid-column: 1/3;
 }
-
 .btn {
   background-color: #f2f2f2;
   border: 1px solid #999;
 }
-
 .operator {
   background-color: orange;
   color: white;
